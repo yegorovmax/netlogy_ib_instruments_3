0f2def5b430aa3e7dc12f40a5c843e635c576568
max_egorov <max.egorov@sureprep.com>
2022-02-10 10:32:21 -0800

    Initial Commit

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Max Egorov on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
