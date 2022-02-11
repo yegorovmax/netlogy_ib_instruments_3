8ce6485215c70b70734e5ec741ebc3508f5a910b
max_egorov <max.egorov@sureprep.com>
2022-02-10 18:07:30 -0800

    finaly seems to be working..

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -9,4 +9,48 @@ import UIKit
 
 class ProfileView: UIView {
     
+    @IBOutlet weak var profileImgUIImageView: UIImageView!
+    
+    @IBOutlet weak var nameUILabel: UILabel!
+    
+    @IBOutlet weak var dobUILabel: UILabel!
+    
+    @IBOutlet weak var textUITaxtView: UITextView!
+    @IBOutlet weak var cityUILabel: UILabel!
+ 
+    let nibName = "ProfileView"
+       var contentView: UIView?
+    required init?(coder aDecoder: NSCoder) {
+            super.init(coder: aDecoder)
+            guard let view = loadViewFromNib() else { return }
+            self.addSubview(view)
+            view.frame = self.bounds
+            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            contentView = view
+        }
+        func loadViewFromNib() -> UIView? {
+            let bundle = Bundle(for: type(of: self))
+            let nib = UINib(nibName: nibName, bundle: bundle)
+            return nib.instantiate(withOwner: self, options: nil).first as? UIView
+        }
+ /*
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupView()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setupView()
+    }
+    private func setupView() {
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? UIView else { return UIView() }
+    return view
+    }*/
+  
 }
